a26d15274627a749877d033b7b223dc918e560b0
Ron Triepels <rontriepels@outlook.com>
2021-05-26 15:13:41 +0200

    Improve package tests.

--- test/runtests.jl
+++ test/runtests.jl
@@ -22,10 +22,8 @@ x = rand(2, 10)
 
 search = ExhaustiveSearch(a=1:2, b=3:4)
 
-resample = RandomSplit(x, ratio=0.8)
+cv = crossvalidate(mymodel, KFold(x), search)
 
-cv = crossvalidate(mymodel, resample, search)
-
-cv = crossvalidate((x) -> mymodel(x, 1, 2), resample)
+cv = crossvalidate((x) -> mymodel(x, 1, 2), LeavePOut(x))
 
 cv = crossvalidate((x) -> crossvalidate(mymodel, FixedSplit(x), search), RandomSplit(x))
