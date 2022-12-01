449e339d06ea62de30be9a825a6ec89244356a5d
Ron Triepels <rontriepels@outlook.com>
2022-12-01 10:34:00 +0100

    Improved tests.

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,7 +3,7 @@ using CrossValidation
 import CrossValidation: fit!, loss
 
 struct MyModel
-    a::Int
+    a::Float64
     b::Float64
 end
 
@@ -11,33 +11,35 @@ function fit!(model::MyModel, x::AbstractArray; epochs::Int = 1)
     return model
 end
 
+# Himmelblau's function
 function loss(model::MyModel, x::AbstractArray)
-    return model.a^2 + model.b^2
+    a, b = model.a, model.b
+    return (a^2 + b - 11)^2 + (a + b^2 - 7)^2
 end
 
 x = rand(2, 100)
 
-validate(MyModel(1, 2.0), (epochs=100,), FixedSplit(x))
-validate(MyModel(1, 2.0), (epochs=100,), RandomSplit(x))
-validate(MyModel(1, 2.0), (epochs=100,), KFold(x))
-validate(MyModel(1, 2.0), (epochs=100,), ForwardChaining(x, 40, 10))
-validate(MyModel(1, 2.0), (epochs=100,), SlidingWindow(x, 40, 10))
+validate(MyModel(2.0, 2.0), (epochs=100,), FixedSplit(x))
+validate(MyModel(2.0, 2.0), (epochs=100,), RandomSplit(x))
+validate(MyModel(2.0, 2.0), (epochs=100,), KFold(x))
+validate(MyModel(2.0, 2.0), (epochs=100,), ForwardChaining(x, 40, 10))
+validate(MyModel(2.0, 2.0), (epochs=100,), SlidingWindow(x, 40, 10))
 
-space = ParameterSpace(a = -10:10, b = -10.0:0.5:10.0)
+space = ParameterSpace(a = -6.0:0.5:6.0, b = -6.0:0.5:6.0)
 
-model = brute(MyModel, GridSampler(space), (epochs=100,), FixedSplit(x))
-model = brute(MyModel, RandomSampler(space, n=100), (epochs=100,), FixedSplit(x))
-model = hc(MyModel, space, (epochs=100,), FixedSplit(x))
+model = brute(MyModel, GridSampler(space), (epochs=100,), FixedSplit(x), maximize=false)
+model = brute(MyModel, RandomSampler(space, n=100), (epochs=100,), FixedSplit(x), maximize=false)
+model = hc(MyModel, space, (epochs=100,), FixedSplit(x), maximize=false)
 
-model = sha(MyModel, GridSampler(space), ConstantBudget((epochs=100,)), FixedSplit(x))
-model = sha(MyModel, RandomSampler(space, n=100), GeometricBudget((epochs=100,), 1.5), FixedSplit(x))
+model = sha(MyModel, GridSampler(space), ConstantBudget((epochs=100,)), FixedSplit(x), maximize=false)
+model = sha(MyModel, RandomSampler(space, n=100), GeometricBudget((epochs=100,), 1.5), FixedSplit(x), maximize=false)
 
 f(train) = train ./ 10
 f(train, test) = train ./ 10, test ./ 10
 
-scores = validate(MyModel(1, 2.0), (epochs=100,), PreProcess(FixedSplit(x), f))
-model = brute(MyModel, RandomSampler(space, n=100), (epochs=100,), PreProcess(KFold(x), f))
+scores = validate(MyModel(2.0, 2.0), (epochs=100,), PreProcess(FixedSplit(x), f))
+model = brute(MyModel, RandomSampler(space, n=100), (epochs=100,), PreProcess(KFold(x), f), maximize=false)
 
-scores = validate(x -> brute(MyModel, GridSampler(space), (epochs=100,), FixedSplit(x)), KFold(x))
-scores = validate(x -> hc(MyModel, space, (epochs=100,), FixedSplit(x)), KFold(x))
-scores = validate(x -> sha(MyModel, GridSampler(space), ConstantBudget((epochs=100,)), FixedSplit(x)), KFold(x))
+scores = validate(x -> brute(MyModel, GridSampler(space), (epochs=100,), FixedSplit(x), maximize=false), KFold(x))
+scores = validate(x -> hc(MyModel, space, (epochs=100,), FixedSplit(x), maximize=false), KFold(x))
+scores = validate(x -> sha(MyModel, GridSampler(space), ConstantBudget((epochs=100,)), FixedSplit(x), maximize=false), KFold(x))
