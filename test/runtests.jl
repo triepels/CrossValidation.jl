f2ad5ba6b70bb02d3eea83211f649a88039ecca1
Ron Triepels <rontriepels@outlook.com>
2022-12-01 11:53:29 +0100

    Improved tests.

--- test/runtests.jl
+++ test/runtests.jl
@@ -27,19 +27,19 @@ validate(MyModel(2.0, 2.0), (epochs=100,), SlidingWindow(x, 40, 10))
 
 space = ParameterSpace(a = -6.0:0.5:6.0, b = -6.0:0.5:6.0)
 
-model = brute(MyModel, GridSampler(space), (epochs=100,), FixedSplit(x), maximize=false)
-model = brute(MyModel, RandomSampler(space, n=100), (epochs=100,), FixedSplit(x), maximize=false)
-model = hc(MyModel, space, (epochs=100,), FixedSplit(x), maximize=false)
+brute(MyModel, GridSampler(space), (epochs=100,), FixedSplit(x), maximize=false)
+brute(MyModel, RandomSampler(space, n=100), (epochs=100,), FixedSplit(x), maximize=false)
+hc(MyModel, space, (epochs=100,), FixedSplit(x), maximize=false)
 
-model = sha(MyModel, GridSampler(space), ConstantBudget((epochs=100,)), FixedSplit(x), maximize=false)
-model = sha(MyModel, RandomSampler(space, n=100), GeometricBudget((epochs=100,), 1.5), FixedSplit(x), maximize=false)
+sha(MyModel, GridSampler(space), ConstantBudget((epochs=100,)), FixedSplit(x), maximize=false)
+sha(MyModel, RandomSampler(space, n=100), GeometricBudget((epochs=100,), 1.5), FixedSplit(x), maximize=false)
 
 f(train) = train ./ 10
 f(train, test) = train ./ 10, test ./ 10
 
-scores = validate(MyModel(2.0, 2.0), (epochs=100,), PreProcess(FixedSplit(x), f))
-model = brute(MyModel, RandomSampler(space, n=100), (epochs=100,), PreProcess(KFold(x), f), maximize=false)
+validate(MyModel(2.0, 2.0), (epochs=100,), PreProcess(FixedSplit(x), f))
+brute(MyModel, RandomSampler(space, n=100), (epochs=100,), PreProcess(KFold(x), f), maximize=false)
 
-scores = validate(x -> brute(MyModel, GridSampler(space), (epochs=100,), FixedSplit(x), maximize=false), KFold(x))
-scores = validate(x -> hc(MyModel, space, (epochs=100,), FixedSplit(x), maximize=false), KFold(x))
-scores = validate(x -> sha(MyModel, GridSampler(space), ConstantBudget((epochs=100,)), FixedSplit(x), maximize=false), KFold(x))
+validate(x -> brute(MyModel, GridSampler(space), (epochs=100,), FixedSplit(x), maximize=false), KFold(x))
+validate(x -> hc(MyModel, space, (epochs=100,), FixedSplit(x), maximize=false), KFold(x))
+validate(x -> sha(MyModel, GridSampler(space), ConstantBudget((epochs=100,)), FixedSplit(x), maximize=false), KFold(x))
