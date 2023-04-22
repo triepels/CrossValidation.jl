0dae4280f1579d284f935a1c7d0015bf80ff63d1
Ron Triepels <rontriepels@outlook.com>
2023-04-22 16:01:20 +0200

    Improved schedule of HyperBudget.

--- src/CrossValidation.jl
+++ src/CrossValidation.jl
@@ -508,7 +508,7 @@ function schedule(budget::HyperBudget{names, T}, b, n, rate) where {names, T}
     for i in OneTo(b)
         c = rate^(i - b)
         args = NamedTuple{names, T}(map(x -> _cast(typeof(x), c * x, RoundNearest), budget.args))
-        brkt[i] = (ceil(Int, n * rate^-i), args)
+        brkt[i] = (ceil(Int, n / rate^i), args)
     end
     return brkt
 end
