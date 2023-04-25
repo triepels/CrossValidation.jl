7647948354a221f8e6ee4f33a15ac127840bb688
Ron Triepels <rontriepels@outlook.com>
2023-04-25 14:38:33 +0200

    Fixed error in HyperBudget.

--- src/CrossValidation.jl
+++ src/CrossValidation.jl
@@ -499,7 +499,7 @@ function schedule(budget::HyperBudget{names, T}, b, n, rate) where {names, T}
     for i in OneTo(b)
         c = rate^(i - b)
         args = NamedTuple{names, T}(map(x -> _cast(typeof(x), c * x, RoundNearest), budget.args))
-        brkt[i] = (floor(Int, n / rate^i), args)
+        brkt[i] = (max(floor(Int, n / rate^i), 1), args)
     end
     return brkt
 end
