f35dde70035d60d1fffcf3e67e4b00d303ce50a4
Ron Triepels <rontriepels@outlook.com>
2023-06-19 11:57:16 +0200

    Improved package tests.

--- test/runtests.jl
+++ test/runtests.jl
@@ -19,46 +19,47 @@ function loss(model::MyModel, x::AbstractArray)
     return (a^2 + b - 11)^2 + (a + b^2 - 7)^2
 end
 
-collect(FixedSplit(1:10))
-collect(RandomSplit(1:10))
+collect(FixedSplit(1:10, 0.8))
+collect(RandomSplit(1:10, 0.8))
 collect(LeaveOneOut(1:10))
-collect(KFold(1:10))
+collect(KFold(1:10, 10))
 collect(ForwardChaining(1:10, 4, 2))
 collect(SlidingWindow(1:10, 4, 2))
 
 x = rand(2, 100)
+data = FixedSplit(x, 0.8)
 
-validate(MyModel(a = 2.0, b = 2.0), FixedSplit(x), args=(epochs = 100,))
+validate(MyModel(a = 2.0, b = 2.0), data, args=(epochs = 100,))
 
 sp = space(a = DiscreteUniform(-8.0:1.0:8.0), b = DiscreteUniform(-8.0:1.0:8.0))
 
-brute(MyModel, sp, FixedSplit(x), args = (epochs = 100,), maximize = false)
-brute(MyModel, sample(sp, 64), FixedSplit(x), args = (epochs = 100,), maximize = false)
-hc(MyModel, sp, FixedSplit(x), args = (epochs = 100,), nstart = 10, k = 1, maximize = false)
+brute(MyModel, sp, data, args = (epochs = 100,), maximize = false)
+brute(MyModel, sample(sp, 64), data, args = (epochs = 100,), maximize = false)
+hc(MyModel, sp, data, args = (epochs = 100,), nstart = 10, k = 1, maximize = false)
 
-sha(MyModel, sp, FixedSplit(x), Budget(epochs = 448), mode = GeometricSchedule, rate = 2, maximize = false)
-sha(MyModel, sample(sp, 64), FixedSplit(x), Budget(epochs = 600), mode = ConstantSchedule, rate = 2, maximize = false)
+sha(MyModel, sp, data, Budget(epochs = 448), mode = GeometricSchedule, rate = 2, maximize = false)
+sha(MyModel, sample(sp, 64), data, Budget(epochs = 600), mode = ConstantSchedule, rate = 2, maximize = false)
 
-hyperband(MyModel, sp, FixedSplit(x), Budget(epochs = 81), rate = 3, maximize = false)
+hyperband(MyModel, sp, data, Budget(epochs = 81), rate = 3, maximize = false)
 
-sasha(MyModel, sp, FixedSplit(x), args = (epochs = 1,), temp = 1, maximize = false)
+sasha(MyModel, sp, data, args = (epochs = 1,), temp = 1, maximize = false)
 
-validate(KFold(x)) do train
-    prms = brute(MyModel, sp, FixedSplit(train), args = (epochs = 100,), maximize = false)
+validate(KFold(x, 10)) do train
+    prms = brute(MyModel, sp, FixedSplit(train, 0.8), args = (epochs = 100,), maximize = false)
     return fit!(MyModel(; prms...), train, epochs = 10)
 end
 
-validate(KFold(x)) do train
-    prms = hc(MyModel, sp, FixedSplit(train), args = (epochs = 100,), nstart = 10, k = 1, maximize = false)
+validate(KFold(x, 10)) do train
+    prms = hc(MyModel, sp, FixedSplit(train, 0.8), args = (epochs = 100,), nstart = 10, k = 1, maximize = false)
     return fit!(MyModel(; prms...), train, epochs = 10)
 end
 
-validate(KFold(x)) do train
-    prms = sha(MyModel, sp, FixedSplit(train), Budget(epochs = 100), mode = GeometricSchedule, rate = 2, maximize = false)
+validate(KFold(x, 10)) do train
+    prms = sha(MyModel, sp, FixedSplit(train, 0.8), Budget(epochs = 100), mode = GeometricSchedule, rate = 2, maximize = false)
     return fit!(MyModel(; prms...), train, epochs = 10)
 end
 
-validate(KFold(x)) do train
-    prms = sasha(MyModel, sp, FixedSplit(train), args = (epochs = 1,), temp = 1, maximize = false)
+validate(KFold(x, 10)) do train
+    prms = sasha(MyModel, sp, FixedSplit(train, 0.8), args = (epochs = 1,), temp = 1, maximize = false)
     return fit!(MyModel(; prms...), train, epochs = 10)
 end
