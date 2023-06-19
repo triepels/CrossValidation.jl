40e37b54187b49c5ed5fba3f32147f4871471477
Ron Triepels <rontriepels@outlook.com>
2023-06-19 15:20:24 +0200

    Improved budget allocation.

--- src/CrossValidation.jl
+++ src/CrossValidation.jl
@@ -9,7 +9,7 @@ import Random: rand
 export AbstractResampler, FixedSplit, RandomSplit, LeaveOneOut, KFold, ForwardChaining, SlidingWindow,
        AbstractSpace, FiniteSpace, InfiniteSpace, space, ParameterVector,
        AbstractDistribution, DiscreteDistribution, ContinousDistribution, Discrete, DiscreteUniform, Uniform, LogUniform, Normal, sample,
-       Budget, ScheduleMode, GeometricSchedule, ConstantSchedule, HyperbandSchedule, schedule,
+       Budget, AllocationMode, GeometricAllocation, ConstantAllocation, HyperbandAllocation, allocate,
        fit!, loss, validate, brute, hc, sha, hyperband, sasha
 
 nobs(x) = length(x)
@@ -465,72 +465,77 @@ function hc(T::Type, space::FiniteSpace, data::AbstractResampler; args::NamedTup
     return parm
 end
 
-struct Budget{names, T<:Tuple{Vararg{Real}}}
-    args::T
-    function Budget(; args...)
-        return new{keys(args), typeof(values(values(args)))}(values(values(args)))
+struct Budget{name, T<:Real}
+    val::T
+    function Budget{name}(val::Real) where name
+        return new{name, typeof(val)}(val)
     end
+end
+
+function Budget(; vars...)
+    length(vars) == 1 || throw(ArgumentError("budget can be specified for only one argument"))
+    return Budget{first(keys(vars))}(first(values(values(vars))))
 end
 
 _cast(::Type{T}, x::Real, r) where T <: Real = T(x)
 _cast(::Type{T}, x::AbstractFloat, r) where T <: Integer = round(T, x, r)
 _cast(::Type{T}, x::T, r) where T <: Real = x
 
-struct ScheduleMode{T} end
+struct AllocationMode{T} end
 
-const GeometricSchedule = ScheduleMode{:Geometric}()
-const ConstantSchedule = ScheduleMode{:Constant}()
-const HyperbandSchedule = ScheduleMode{:Hyperband}()
+const GeometricAllocation = AllocationMode{:Geometric}()
+const ConstantAllocation = AllocationMode{:Constant}()
+const HyperbandAllocation = AllocationMode{:Hyperband}()
 
-function schedule(budget::Budget{names, T}, mode::ScheduleMode{:Geometric}, narms::Int, rate::Real) where {names, T}
+function allocate(budget::Budget, mode::AllocationMode{:Geometric}, narms::Int, rate::Real)
     nrounds = floor(Int, log(rate, narms)) + 1
-    return schedule(budget, mode, nrounds, narms, rate)
+    return allocate(budget, mode, nrounds, narms, rate)
 end
 
-function schedule(budget::Budget{names, T}, mode::ScheduleMode{:Geometric}, nrounds::Int, narms::Int, rate::Real) where {names, T}
+function allocate(budget::Budget{name, T}, mode::AllocationMode{:Geometric}, nrounds::Int, narms::Int, rate::Real) where {name, T}
     arms = Vector{Int}(undef, nrounds)
-    args = Vector{NamedTuple{names, T}}(undef, nrounds)
+    args = Vector{NamedTuple{(name,), Tuple{T}}}(undef, nrounds)
     for i in OneTo(nrounds)
         c = 1 / (round(Int, narms / rate^(i - 1)) * nrounds)
-        args[i] = NamedTuple{names, T}(map(x -> _cast(typeof(x), c * x, RoundDown), budget.args))
+        args[i] = NamedTuple{(name,)}(_cast(typeof(budget.val), c * budget.val, RoundDown))
         arms[i] = ceil(Int, narms / rate^i)
     end
     return zip(arms, args)
 end
 
-function schedule(budget::Budget{names, T}, mode::ScheduleMode{:Constant}, narms::Int, rate::Real) where {names, T}
+function allocate(budget::Budget, mode::AllocationMode{:Constant}, narms::Int, rate::Real)
     nrounds = floor(Int, log(rate, narms)) + 1
-    return schedule(budget, mode, nrounds, narms, rate)
+    return allocate(budget, mode, nrounds, narms, rate)
 end
 
-function schedule(budget::Budget{names, T}, mode::ScheduleMode{:Constant}, nrounds::Int, narms::Int, rate::Real) where {names, T}
+function allocate(budget::Budget{name, T}, mode::AllocationMode{:Constant}, nrounds::Int, narms::Int, rate::Real) where {name, T}
     arms = Vector{Int}(undef, nrounds)
-    args = Vector{NamedTuple{names, T}}(undef, nrounds)
+    args = Vector{NamedTuple{(name,), Tuple{T}}}(undef, nrounds)
     c = (rate - 1) * rate^(nrounds - 1) / (narms * (rate^nrounds - 1))
     for i in OneTo(nrounds)
-        args[i] = NamedTuple{names, T}(map(x -> _cast(typeof(x), c * x, RoundDown), budget.args))
+        args[i] = NamedTuple{(name,)}(_cast(typeof(budget.val), c * budget.val, RoundDown))
         arms[i] = ceil(Int, narms / rate^i)
     end
     return zip(arms, args)
 end
 
-function schedule(budget::Budget{names, T}, mode::ScheduleMode{:Hyperband}, narms::Int, rate::Real) where {names, T}
-    nrounds = floor(Int, log(rate, first(budget.args))) + 1
-    return schedule(budget, mode, nrounds, narms, rate)
+function allocate(budget::Budget, mode::AllocationMode{:Hyperband}, narms::Int, rate::Real)
+    nrounds = floor(Int, log(rate, budget.val)) + 1
+    return allocate(budget, mode, nrounds, narms, rate)
 end
 
-function schedule(budget::Budget{names, T}, mode::ScheduleMode{:Hyperband}, nrounds::Int, narms::Int, rate::Real) where {names, T}
+function allocate(budget::Budget{name, T}, mode::AllocationMode{:Hyperband}, nrounds::Int, narms::Int, rate::Real) where {name, T}
     arms = Vector{Int}(undef, nrounds)
-    args = Vector{NamedTuple{names, T}}(undef, nrounds)
+    args = Vector{NamedTuple{(name,), Tuple{T}}}(undef, nrounds)
     for i in OneTo(nrounds)
         c = 1 / rate^(nrounds - i)
-        args[i] = NamedTuple{names, T}(_cast(typeof(first(budget.args)), c * first(budget.args), RoundNearest)) #RoundNearest?
+        args[i] = NamedTuple{(name,)}(_cast(typeof(budget.val), c * budget.val, RoundNearest)) #RoundNearest?
         arms[i] = max(floor(Int, narms / rate^i), 1)
     end
     return zip(arms, args)
 end
 
-function sha(T::Type, prms::ParameterVector, data::AbstractResampler, budget::Budget; mode::ScheduleMode = GeometricSchedule, rate::Number = 2, maximize::Bool = false)
+function sha(T::Type, prms::ParameterVector, data::AbstractResampler, budget::Budget; mode::AllocationMode = GeometricSchedule, rate::Number = 2, maximize::Bool = false)
     length(prms) ≥ 1 || throw(ArgumentError("nothing to optimize"))
     length(data) == 1 || throw(ArgumentError("can only optimize over one resample fold"))
     rate > 1 || throw(ArgumentError("unable to discard arms with rate $rate"))
@@ -539,7 +544,7 @@ function sha(T::Type, prms::ParameterVector, data::AbstractResampler, budget::Bu
     arms = map(x -> T(; x...), prms)
 
     @debug "Start successive halving"
-    for (k, args) in schedule(budget, mode, length(arms), rate)
+    for (k, args) in allocate(budget, mode, length(arms), rate)
         arms = pmap(x -> _fit!(x, train, args), arms)
         loss = map(x -> _loss(x, test), arms)
         @debug "Validated arms" prms args loss
@@ -553,7 +558,7 @@ function sha(T::Type, prms::ParameterVector, data::AbstractResampler, budget::Bu
     return first(prms)
 end
 
-sha(T::Type, space::FiniteSpace, data::AbstractResampler, budget::Budget; mode::ScheduleMode = GeometricSchedule, rate::Number = 2, maximize::Bool = false) =
+sha(T::Type, space::FiniteSpace, data::AbstractResampler, budget::Budget; mode::AllocationMode = GeometricSchedule, rate::Number = 2, maximize::Bool = false) =
     sha(T, collect(space), data, budget, mode = mode, rate = rate, maximize = maximize)
 
 function hyperband(T::Type, space::AbstractSpace, data::AbstractResampler, budget::Budget; rate::Number = 3, maximize::Bool = false)
@@ -564,7 +569,7 @@ function hyperband(T::Type, space::AbstractSpace, data::AbstractResampler, budge
     best = maximize ? -Inf : Inf
 
     train, test = first(data)
-    n = floor(Int, log(rate, first(budget.args))) + 1
+    n = floor(Int, log(rate, first(budget.val))) + 1
 
     @debug "Start hyperband"
     for i in reverse(OneTo(n))
@@ -575,7 +580,7 @@ function hyperband(T::Type, space::AbstractSpace, data::AbstractResampler, budge
         arms = map(x -> T(; x...), prms)
 
         @debug "Start successive halving"
-        for (k, args) in schedule(budget, HyperbandSchedule, i, narms, rate)
+        for (k, args) in allocate(budget, HyperbandAllocation, i, narms, rate)
             arms = pmap(x -> _fit!(x, train, args), arms)
             loss = map(x -> _loss(x, test), arms)
             @debug "Validated arms" prms args loss
