be7117c636ba7e4dee09db0d00f7aa46f5b5ae3a
Ron Triepels <rontriepels@outlook.com>
2023-10-09 13:31:05 +0200

    Updated package tests.

--- test/runtests.jl
+++ test/runtests.jl
@@ -34,13 +34,13 @@ validate(MyModel(a = 2.0, b = 2.0), data, args=(epochs = 100,))
 sp = space(a = DiscreteUniform(-8.0:1.0:8.0), b = DiscreteUniform(-8.0:1.0:8.0))
 
 brute(MyModel, sp, data, args = (epochs = 100,), maximize = false)
-brute_fit(MyModel, sp, data, args = (epochs = 100,), maximize = false)
+brutefit(MyModel, sp, data, args = (epochs = 100,), maximize = false)
 
 brute(MyModel, sample(sp, 64), data, args = (epochs = 100,), maximize = false)
-brute_fit(MyModel, sample(sp, 64), data, args = (epochs = 100,), maximize = false)
+brutefit(MyModel, sample(sp, 64), data, args = (epochs = 100,), maximize = false)
 
-hc(MyModel, sp, data, args = (epochs = 100,), nstart = 10, k = 1, maximize = false)
-hc_fit(MyModel, sp, data, args = (epochs = 100,), nstart = 10, k = 1, maximize = false)
+hc(MyModel, sp, data, 1, args = (epochs = 100,), n = 10, maximize = false)
+hcfit(MyModel, sp, data, 1, args = (epochs = 100,), n = 10, maximize = false)
 
 sha(MyModel, sp, data, Budget{:epochs}(448), mode = GeometricAllocation, rate = 2, maximize = false)
 sha_fit(MyModel, sp, data, Budget{:epochs}(448), mode = GeometricAllocation, rate = 2, maximize = false)
@@ -60,7 +60,7 @@ validate(KFold(x, 10)) do train
 end
 
 validate(KFold(x, 10)) do train
-    parm = hc(MyModel, sp, FixedSplit(train, 0.8), args = (epochs = 100,), nstart = 10, k = 1, maximize = false)
+    parm = hc(MyModel, sp, FixedSplit(train, 0.8), 1, args = (epochs = 100,), n = 10, maximize = false)
     return fit!(MyModel(; parm...), train, epochs = 10)
 end
 
@@ -75,11 +75,11 @@ validate(KFold(x, 10)) do train
 end
 
 validate(KFold(x, 10)) do train
-    return brute_fit(MyModel, sp, FixedSplit(train, 0.8), args = (epochs = 100,), maximize = false)
+    return brutefit(MyModel, sp, FixedSplit(train, 0.8), args = (epochs = 100,), maximize = false)
 end
 
 validate(KFold(x, 10)) do train
-    return hc_fit(MyModel, sp, FixedSplit(train, 0.8), args = (epochs = 100,), nstart = 10, k = 1, maximize = false)
+    return hcfit(MyModel, sp, FixedSplit(train, 0.8), 1, args = (epochs = 100,), n = 10, maximize = false)
 end
 
 validate(KFold(x, 10)) do train
